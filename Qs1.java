7ffecb17c891c6b742a22cc91434c4f371b681d6
AmanMaheshSingh <amanmaheshsingh@gmail.com>
2025-08-31 22:11:35 +0530

    Added Qs1 and Qs2

---
+++ Qs1.java
@@ -0,0 +1,8 @@
+//You must print two lines of output:    1>Print Hello, World. on the first line.   2>Print Hello, Java. on the second line.
+//HackerRank Java Prepare Question no 1
+class Qs1{
+
+    public static void main(String[] args) {
+        System.out.print("Hello, World.\nHello, Java.");
+    }
+}
